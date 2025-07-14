66f0d94a08c3d3575c64f860ac24a5959b9b542f
thomve <thomas.veran@gmail.com>
2025-07-14 11:41:50 +0200

    added gradio interface

--- src/agent_w_visual_tool.py
+++ src/agent_w_visual_tool.py
@@ -1,14 +1,41 @@
-from smolagents import CodeAgent, InferenceClientModel, Tool
+import gradio as gr
+
+from smolagents import CodeAgent, InferenceClientModel, Tool, stream_to_gradio
 
 image_generation_tool = Tool.from_space(
     "black-forest-labs/FLUX.1-schnell",
     name="image_generator",
-    description="Generate an image from a prompt"
+    description="Generates an image following your prompt. Returns a PIL Image.",
+    api_name="/infer"
 )
 
 model = InferenceClientModel(model_id="Qwen/Qwen2.5-Coder-7B-Instruct")
 agent = CodeAgent(tools=[image_generation_tool], model=model)
 
-agent.run(
-    "Improve this prompt, then generate an image of it.", additional_args={'user_prompt': 'A rabbit wearing a space suit'}
+def interact_with_agent(prompt, history):
+    messages = []
+    yield messages
+    for msg in stream_to_gradio(agent, prompt):
+        messages.append(msg)
+        yield messages
+    yield messages
+
+
+demo = gr.ChatInterface(
+    interact_with_agent,
+    chatbot= gr.Chatbot(
+        label="Agent",
+        type="messages",
+        avatar_images=(
+            None,
+            "https://em-content.zobj.net/source/twitter/53/robot-face_1f916.png",
+        ),
+    ),
+    examples=[
+        ["Generate an image of an astronaut riding an alligator"],
+        ["I am writing a children's book for my daughter. Can you help me with some illustrations?"],
+    ],
+    type="messages",
 )
+
+demo.launch()
